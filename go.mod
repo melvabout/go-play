a33b8d9905e87fba599a4fb10c0193ade7ceac74
Andrew Melvin <andy@aomit.co.uk>
2025-04-13 16:35:12 +0100

    Hello

---
+++ go.mod
@@ -0,0 +1,3 @@
+module melvabout/go-play
+
+go 1.24.2
---
+++ hello.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+
+// This is a simple Go program that prints "Hello, World!" to the console.
+func main() {
+	fmt.Println("Hello, World!")
+}
